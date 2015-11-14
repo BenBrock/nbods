82ad18d77b6f9f15722e98046b43ab44a28da786
Benjamin Brock <brock@utk.edu>
2015-11-14 15:15:03 -0500

    [5mFixed indentation[0m

--- src/phys.cpp
+++ src/phys.cpp
@@ -8,8 +8,8 @@
 
 
 typedef struct {
-    f2 pos;
-    f2 vel;
+  f2 pos;
+  f2 vel;
 } Particle;
 
 
@@ -17,30 +17,33 @@ Particle *particles;
 int N;
 
 
-void phys_init(int n) {
-    N = n;
-    particles = calloc(N, sizeof(Particle));
-    for (int i = 0; i < N; i++) {
-        particles[i].pos.x = (double) rand() / RAND_MAX;
-        particles[i].pos.y = (double) rand() / RAND_MAX;
-        particles[i].vel.x = (double) rand() / RAND_MAX - 0.5;
-        particles[i].vel.y = (double) rand() / RAND_MAX - 0.5;
-    }
+void phys_init(int n)
+{
+  N = n;
+  particles = calloc(N, sizeof(Particle));
+  for (int i = 0; i < N; i++) {
+    particles[i].pos.x = (double) rand() / RAND_MAX;
+    particles[i].pos.y = (double) rand() / RAND_MAX;
+    particles[i].vel.x = (double) rand() / RAND_MAX - 0.5;
+    particles[i].vel.y = (double) rand() / RAND_MAX - 0.5;
+  }
 }
 
-void phys_step(double dt) {
-    for (int i = 0; i < N; i++) {
-        f2 acc = {0, 0};
-        for (int j = 0; j < N; j++) {
-        }
-        acc = f2_mult(acc, G);
-        particles[i].vel = f2_add(particles[i].vel, f2_mult(acc, dt));
-        particles[i].pos = f2_add(particles[i].pos, f2_mult(particles[i].vel, dt));
+void phys_step(double dt)
+{
+  for (int i = 0; i < N; i++) {
+    f2 acc = {0, 0};
+    for (int j = 0; j < N; j++) {
     }
+    acc = f2_mult(acc, G);
+    particles[i].vel = f2_add(particles[i].vel, f2_mult(acc, dt));
+    particles[i].pos = f2_add(particles[i].pos, f2_mult(particles[i].vel, dt));
+  }
 }
 
-void phys_print() {
-    for (int i = 0; i < N; i++) {
-        printf("pos: %lg, %lg\tvel: %lg, %lg\n", particles[i].pos.x, particles[i].pos.y, particles[i].vel.x, particles[i].vel.y);
-    }
+void phys_print()
+{
+  for (int i = 0; i < N; i++) {
+    printf("pos: %lg, %lg\tvel: %lg, %lg\n", particles[i].pos.x, particles[i].pos.y, particles[i].vel.x, particles[i].vel.y);
+  }
 }
