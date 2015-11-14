1d086dabaf01f21794baca50799f9f9a60d7aa6a
Andrew Belt <andrewpbelt@gmail.com>
2015-11-14 14:38:28 -0500

    inlined vecs

--- src/vec.h
+++ src/vec.h
@@ -6,31 +6,31 @@ typedef struct {
 } f2;
 
 
-f2 f2_add(f2 a, f2 b) {
+inline f2 f2_add(f2 a, f2 b) {
 	return (f2) {a.x + b.x, a.y + b.y};
 }
 
-f2 f2_minus(f2 a, f2 b) {
+inline f2 f2_minus(f2 a, f2 b) {
 	return (f2) {a.x - b.x, a.y - b.y};
 }
 
-f2 f2_mult(f2 a, double s) {
+inline f2 f2_mult(f2 a, double s) {
 	return (f2) {a.x * s, a.y * s};
 }
 
-f2 f2_div(f2 a, double s) {
+inline f2 f2_div(f2 a, double s) {
 	return (f2) {a.x / s, a.y / s};
 }
 
-double f2_norm(f2 a) {
+inline double f2_norm(f2 a) {
 	return hypot(a.x, a.y);
 }
 
-// Assumes a is not zero
-f2 f2_normalize(f2 a) {
+/* Assumes a is not zero */
+inline f2 f2_normalize(f2 a) {
 	return f2_div(a, f2_norm(a));
 }
 
-double f2_dot(f2 a, f2 b) {
+inline double f2_dot(f2 a, f2 b) {
 	return a.x*b.x + a.y*b.y;
 }
