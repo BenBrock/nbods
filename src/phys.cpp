ecdd2fde1bd91c65a995c98875aecc9768ee83f5
Andrew Belt <andrewpbelt@gmail.com>
2015-11-14 19:15:12 -0500

    Add energy normalization and wall bouncing

--- src/phys.cpp
+++ src/phys.cpp
@@ -5,10 +5,32 @@
 #include "phys.hpp"
 
 /* gravitational constant */
-#define G 1.0
+#define G 1.0e-3
+#define RMIN 1.0e-2
 
 Particle *particles;
 int N;
+double E0;
+
+double get_energy()
+{
+  double E = 0.0;
+  for (int i = 0; i < N; i++) {
+    // kinetic
+    double Ei = 0.5 * f2_dot(particles[i].vel, particles[i].vel);
+
+    // potential
+    for (int j = 0; j < N; j++) {
+      if (i == j)
+        continue;
+      f2 r = f2_minus(particles[i].pos, particles[j].pos);
+      double r_norm = f2_norm(r);
+      Ei -= G / r_norm;
+    }
+    E += Ei;
+  }
+  return E;
+}
 
 void phys_init(int n)
 {
@@ -20,6 +42,7 @@ void phys_init(int n)
     particles[i].vel.x = (double) rand() / RAND_MAX - 0.5;
     particles[i].vel.y = (double) rand() / RAND_MAX - 0.5;
   }
+  E0 = get_energy();
 }
 
 void phys_step(double dt)
@@ -27,13 +50,47 @@ void phys_step(double dt)
   for (int i = 0; i < N; i++) {
     f2 acc = {0, 0};
     for (int j = 0; j < N; j++) {
+      if (i == j)
+        continue;
       f2 r = f2_minus(particles[i].pos, particles[j].pos);
-      acc = f2_add(acc, f2_mult(r, pow(f2_norm(r), -3)));
+      double r_norm = f2_norm(r);
+      if (r_norm < RMIN)
+        continue;
+      acc = f2_add(acc, f2_mult(r, -pow(r_norm, -3)));
     }
     acc = f2_mult(acc, G);
     particles[i].vel = f2_add(particles[i].vel, f2_mult(acc, dt));
     particles[i].pos = f2_add(particles[i].pos, f2_mult(particles[i].vel, dt));
+
+    /* Bounce on walls */
+    if (particles[i].pos.x < 0.0) {
+      particles[i].pos.x *= -1;
+      particles[i].vel.x *= -1;
+    }
+    else if (particles[i].pos.x >= 1.0) {
+      particles[i].pos.x = 2.0 - particles[i].pos.x;
+      particles[i].vel.x *= -1;
+    }
+    if (particles[i].pos.y < 0.0) {
+      particles[i].pos.y *= -1;
+      particles[i].vel.y *= -1;
+    }
+    else if (particles[i].pos.y >= 1.0) {
+      particles[i].pos.y = 2.0 - particles[i].pos.y;
+      particles[i].vel.y *= -1;
+    }
   }
+
+  /* Renormalization */
+  double E = get_energy();
+  for (int i = 0; i < N; i++) {
+    particles[i].vel = f2_mult(particles[i].vel, E / E0);
+  }
+}
+
+double asdf(double r)
+{
+  return 1.0 / r;
 }
 
 void phys_print()
